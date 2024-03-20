0f48d8754f2d5f2a07db0d67d187a62f3d44e51d
mt-rein <m.t.rein@tilburguniversity.edu>
2024-03-20 15:14:24 +0100

    set up new simulation study

---
+++ auxilliary functions.R
@@ -0,0 +1,9 @@
+# This script defines auxiliary functions to be used in the simulation
+# specifically, these functions catch errors and warnings
+run_step1 <- quietly(safely(step1))
+run_step2 <- quietly(safely(step2))
+run_step3 <- quietly(safely(step3))
+run_NFS <- quietly(safely(sem))
+run_SAM <- quietly(safely(sam))
+run_SEM <- quietly(safely(sem))
+run_SEcorrection <- quietly(safely(SEcorrection))
---
+++ step1.R
@@ -0,0 +1,34 @@
+# only for testing!
+# data <- read.csv("examples/exampledata.csv")
+# measurementmodel <- "
+#   f1 =~ v1 + v2 + v3 + v4
+#   f2 =~ v5 + v6 + v7 + v8
+#   v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 ~ 0*1
+#   "
+# source("center_within.R")
+# data <- center_within(data, vars = paste0("v", 1:8), id = "id")
+# id = "id"
+
+step1 <- function(data, measurementmodel, id){
+  # data:
+  #   a data frame with the indicator and ID variables
+  # measurementmodel:
+  #   a string describing the measurement model using the lavaan syntax
+  # id:
+  #   a character that indicates the id variable (the variable that indicates
+  #   which observations belong to which person)
+  
+  library(lavaan)
+  
+  # estimate the measurement model:
+  fit <- cfa(measurementmodel,
+             data = data,
+             orthogonal = TRUE,
+             missing = "ML",
+             cluster = id)
+
+  # assemble output
+  output <- list("fit_step1" = fit,
+                 "data" = data)
+  return(output)
+}
