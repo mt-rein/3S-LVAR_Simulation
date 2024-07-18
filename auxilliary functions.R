9d3ad11e298e4c87ba2fd9b83ea94d0842c49b74
mt-rein <m.t.rein@tilburguniversity.edu>
2024-07-18 18:48:32 +0200

    added function to correct for Nickell's bias

--- auxilliary functions.R
+++ auxilliary functions.R
@@ -1,8 +1,19 @@
 # This script defines auxiliary functions to be used in the simulation
-# specifically, these functions catch errors and warnings
+# these functions catch errors and warnings:
 run_step1 <- quietly(safely(step1))
 run_step2 <- quietly(safely(step2))
 run_step3 <- quietly(safely(step3))
 run_SAM <- quietly(safely(sam))
 run_SEM <- quietly(safely(sem))
 run_SEcorrection <- quietly(safely(stepwiseSE))
+
+# function to approximate Nickell's bias:
+nickells_bias <- function(phi, obs) {
+  k <- ncol(phi) # Number of variables in the VAR model
+  I_k <- diag(k) # Identity matrix of order k
+  
+  # Calculate the bias correction term
+  bias <- -(I_k + phi) / (obs - 1)
+  
+  return(bias)
+}
